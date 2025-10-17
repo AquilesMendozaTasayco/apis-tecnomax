8681ac915e7325eacc9786c3450c6bb1d814bac8
AquilesMendozaTasayco <aquilesfabianmendozatasayco@gmail.com>
2025-10-17 09:11:18 -0500

    Upgrade conexion

--- config/db.php
+++ config/db.php
@@ -1,12 +1,13 @@
 <?php
 header("Content-Type: application/json");
 
-$host = getenv('DB_HOST') ?: 'localhost';
+$host = getenv('DB_HOST') ?: 'mysql.railway.internal';
 $user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: 'root';
-$dbname = getenv('DB_NAME') ?: 'tecnomax';
+$pass = getenv('DB_PASS') ?: 'mfJrKLXdABPHuHCxuFJuUsFlziBVWUCR';
+$dbname = getenv('DB_NAME') ?: 'railway';
+$port = getenv('DB_PORT') ?: 3306;
 
-$conexion = new mysqli($host, $user, $pass, $dbname);
+$conexion = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conexion->connect_error) {
     http_response_code(500);
